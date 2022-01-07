1e053d4f408d424a94d6d935ac92a3235ce14450
Joseph-Reece <ndirashjose@gmail.com>
2022-01-07 22:32:20 +0300

    GA Fix : Add Tag in header

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,9 @@ import { Suspense, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import BaseRoutes from './routes/BaseRoutes';
 import Preloader from './components/UIElements/Preloader/Preloader';
-import ReactGa from 'react-ga';
 
 const App = () => {
-  
-  useEffect(() => {
-    ReactGa.initialize('G-JL5SHBX2BM');
-    ReactGa.pageview(window.location.pathname + window.location.search);
-}, [])
+
 
   return (
     <Suspense fallback={<Preloader />}>
